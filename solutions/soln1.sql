8c4db8a39ef4cd89391bf37939cb3ba682ee7b28
menyagah <menyagah27@gmail.com>
2021-01-15 09:22:55 +0300

    Find the country where the name is the capital city

--- solutions/soln1.sql
+++ solutions/soln1.sql
@@ -80,4 +80,14 @@ SELECT name FROM world
 -- Find the countries that have exactly four characters.
 
 SELECT name FROM world
- WHERE name LIKE '____'
+ WHERE name LIKE '____' 
+
+
+--  11.
+-- The capital of Luxembourg is Luxembourg. Show all the countries where the capital is the same as the name of the country
+
+-- Find the country where the name is the capital city.
+
+SELECT name
+  FROM world
+ WHERE name LIKE capital
