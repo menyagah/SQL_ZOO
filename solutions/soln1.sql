bcde41dc21432382bc4f5487a1a8041c5d248ebf
menyagah <menyagah27@gmail.com>
2021-01-15 09:16:34 +0300

    Find countries that have t as the second character

--- solutions/soln1.sql
+++ solutions/soln1.sql
@@ -53,3 +53,13 @@ SELECT name FROM world
 
 SELECT name FROM world
   WHERE name LIKE '%a%a%a%'
+
+
+-- 8.
+-- India and Angola have an n as the second character. You can use the underscore as a single character wildcard.
+
+-- Find the countries that have "t" as the second character.
+
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
