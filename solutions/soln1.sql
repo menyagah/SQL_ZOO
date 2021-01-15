80aeec3ecc934ac2a0a2fd8ba9c952effd756bea
menyagah <menyagah27@gmail.com>
2021-01-15 09:33:53 +0300

    Find the capital and the name where the capital includes the name of the country

--- solutions/soln1.sql
+++ solutions/soln1.sql
@@ -101,3 +101,10 @@ SELECT name
 SELECT name 
   FROM world
  WHERE capital LIKE concat(name, '_City')
+
+
+-- 13.
+-- Find the capital and the name where the capital includes the name of the country.
+
+SELECT capital, name FROM world
+WHERE capital LIKE concat(name, '%')
