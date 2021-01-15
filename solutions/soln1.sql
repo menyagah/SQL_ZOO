8478f8910c5234cf58dc213a32b14616f552d69f
menyagah <menyagah27@gmail.com>
2021-01-15 09:14:57 +0300

    Add countries that have three or more a in name solution

--- solutions/soln1.sql
+++ solutions/soln1.sql
@@ -45,3 +45,11 @@ SELECT name FROM world
 SELECT name FROM world
   WHERE name LIKE '%oo%'
   
+
+-- 7.
+-- Bahamas has three a - who else?
+
+-- Find the countries that have three or more a in the name
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
