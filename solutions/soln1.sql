894af89cb4f530e669778a493da01a535040e2ca
menyagah <menyagah27@gmail.com>
2021-01-15 09:10:05 +0300

    Add solution to countries that end with land

--- solutions/soln1.sql
+++ solutions/soln1.sql
@@ -20,3 +20,11 @@ SELECT name FROM world
 
 SELECT name FROM world
   WHERE name LIKE '%x%'
+
+--   4.
+-- Iceland, Switzerland end with land - but are there others?
+
+-- Find the countries that end with land
+
+SELECT name FROM world
+  WHERE name LIKE '%land'
