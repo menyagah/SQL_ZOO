3591538908668c63c3c94e766cd5aea9480aa30f
menyagah <menyagah27@gmail.com>
2021-01-16 22:14:16 +0300

    Add solution to show name, continent and population

---
+++ solutions/soln2.sql
@@ -0,0 +1,4 @@
+-- 1.
+-- Read the notes about this table. Observe the result of running this SQL command to show the name, continent and population of all countries.
+
+SELECT name, continent, population FROM world
